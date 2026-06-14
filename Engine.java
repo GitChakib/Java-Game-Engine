1646b7fb840fa93a0f4d9a149ed9e16c6fc44788
ChakibFehdSenoussi <faihasemon12345@gmail.com>
2026-06-14 19:04:42 +0100

    initial

---
+++ Engine.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+
+public class Engine implements Runnable{
+
+    private JFrame window;
+    private Canvas canvas;
+
+    private Thread thread;
+    private boolean running  = false;
+
+
+    private final int width = 800;
+    private final int height = 600;
+
+    private final double fps = 60.0;
+    private final double tickrate = 1.0 / fps;
+
+public Engine() {
+
+    window = new Jframe("Simulation");
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false);
+
+}
+}
