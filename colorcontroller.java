10f1148fa46f79faf60f967ca558804d34b09988
ChakibFehdSenoussi <faihasemon12345@gmail.com>
2026-06-20 23:24:43 +0100

    created color changing prototype

--- colorcontroller.java
+++ colorcontroller.java
@@ -3,7 +3,7 @@ import java.awt.Component;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 
-public class colorcontroller extends Component {
+public class colorController extends Component {
 
     private Input input;
 
@@ -11,8 +11,25 @@ public class colorcontroller extends Component {
     private Color[] colors = {Color.BLACK, Color.WHITE, Color.RED, Color.GREEN, Color.BLUE};
     private int currentColorIndex = 0;
 
+    private boolean wasKeyPressedLastFrame = false;
+
+    public colorController (Input input) {
+    this.input = input;
+}
 
 
+@Override
+public void update(double dt) {
+    boolean isKeyPressed = input.iskeyPressed(KeyEvent.VK_SPACE);
+    if (isKeyPressed && !wasKeyPressedLastFrame) {
+        currentColorIndex = (currentColorIndex + 1) % colors.length;
+    }
+    wasKeyPressedLastFrame = isKeyPressed;
+}
 
-    
+@Override
+public void render(Graphics2D g2d) {
+    g2d.setColor(colors[currentColorIndex]);
+    g2d.fillRect(0, 0, 800, 600);
+}
 }
