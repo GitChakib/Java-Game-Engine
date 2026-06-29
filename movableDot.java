814c60f9e6f92e5fcc0da7d7b7c6298cd55ed1ef
ChakibFehdSenoussi <faihasemon12345@gmail.com>
2026-06-29 20:14:03 +0100

    added movable dot body

--- movableDot.java
+++ movableDot.java
@@ -3,8 +3,28 @@ import java.awt.event.KeyEvent;
 
 
 
-public class movableDot {
+public class movableDot extends Component {
+
+    private Input input;
+    private int posX;
+    private int posY;
+
+@Override
+public void update(double dt) {
+    if (input.isKeyPressed(KeyEvent.VK_W)) {
+        posY -= 5;
+    }
+    if (input.isKeyPressed(KeyEvent.VK_S)) {
+        posY += 5;
+    }
+    if (input.isKeyPressed(KeyEvent.VK_A)) {
+        posX -= 5;
+    }
+    if (input.isKeyPressed(KeyEvent.VK_D)) {
+        posX += 5;
+    }
+
+}
 
 
-    
 }
